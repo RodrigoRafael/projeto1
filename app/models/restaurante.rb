a85ee3c0328257a1a3d3df9dd36bfc613a0b6bc7
RB4 <rb4@rb4-Lenovo-G475.(none)>
2013-01-10 17:45:45 -0200

    finalizandoo

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -12,7 +12,7 @@ class Restaurante < ActiveRecord::Base
 
   has_many :qualificacaos
   has_and_belongs_to_many :pratos
-  has_many :comentarios, as: :comentavel
+  has_many :comentarios, :as => :comentavel
 
       has_attached_file :foto, :styles => { :medium => "300x300>",
                                           :thumb => "100x100>",
